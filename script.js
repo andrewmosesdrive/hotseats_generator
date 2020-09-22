befb84b3cf8f3e06ad142bde8ad66c6896428700
Andrew Moses <andrewmosesdrive@gmail.com>
2020-09-22 10:20:12 -0700

    created intial framework with minimal materialize and some peudocoding for necessary javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(() => {
+// create array for each set of questions based on type, with objects holding question and answer
+let mloQs = [{
+    question: "",
+    answer: ""
+}];
+let gmkQs = [];
+let ethQs = [];
+let uscQs = [];
+let fedlawQs = [];
+
+
+
+// create array for final questions to be randomized and read off
+let finalArray = [];
+
+// function to push choices to finalArray
+function choicePush() {
+
+};
+
+// need click recognition for choices, try boolean? (if clicked === yes)
+
+// need next/back buttons
+
+
+
+
+})
